fda60456f83633d7754c9276de1bb59c5da5730e
qp0kycHuk <serega990112@mail.ru>
2023-01-07 21:27:37 +0300

    add ingridient component

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,19 +1,28 @@
-import {Button} from "../Button/Button";
-import {useState} from "react";
+import { Button } from "../Button/Button";
+import { useState } from "react";
+import { Ingredient } from "../Ingredient/Ingredient";
 
-export const Dish = ({dish}) => {
+export const Dish = ({ dish }) => {
     const [count, setCount] = useState(0);
 
     if (!dish) {
         return null;
     }
 
-    return <div>
-        {dish.name}
-        <div>
-            <Button onClick={() => setCount(count - 1)}>-</Button>
-            {count}
-            <Button onClick={() => setCount(count + 1)}>+</Button>
+    return (<div>
+        <div style={{ display: 'flex', gap: 15 }}>
+            {dish.name}
+            <div>
+                <Button onClick={() => setCount(Math.max(count - 1, 0))}>-</Button>
+                {count}
+                <Button onClick={() => setCount(Math.min(count + 1, 5))}>+</Button>
+            </div>
         </div>
-    </div>
+        {count > 0 ?
+            (<ul>
+                {dish.ingredients?.map((item) => <li><Ingredient ingredient={item} /></li>)}
+            </ul>)
+            : null
+        }
+    </div>)
 }

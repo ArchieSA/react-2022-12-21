7c0c30422fd32be1c2c8f45a1e989c4a0a8a8f8d
Freezoman <serg-tuim@mail.ru>
2023-01-07 23:01:55 +0700

    HW_2

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,9 +1,14 @@
 import {Button} from "../Button/Button";
+import {Ingredients} from "../Ingredients/Ingredients";
 import {useState} from "react";
 
 export const Dish = ({dish}) => {
     const [count, setCount] = useState(0);
 
+    const removeDish = () => {
+        if(count > 0)
+            setCount(count - 1)
+    }
     if (!dish) {
         return null;
     }
@@ -11,9 +16,17 @@ export const Dish = ({dish}) => {
     return <div>
         {dish.name}
         <div>
-            <Button onClick={() => setCount(count - 1)}>-</Button>
+            <Button onClick={removeDish}>
+                -
+            </Button>
             {count}
             <Button onClick={() => setCount(count + 1)}>+</Button>
         </div>
+        {
+            count > 0 && 
+            <Ingredients 
+                ingredients={dish.ingredients}
+            />
+        }
     </div>
 }

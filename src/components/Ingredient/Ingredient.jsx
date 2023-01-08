f23ee12de92f0706f47cc0de965242ca9b952e50
Pham Khanh <zloy_monk@MacBook-Pro-Pham.local>
2023-01-09 00:07:56 +0700

    add hw-2

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -1,23 +1,15 @@
 import {Button} from "../Button/Button";
-import {Ingredients} from "../Ingredients/Ingredients"
 import {useState} from "react";
 
-export const Dish = ({dish}) => {
-    const [count, setCount] = useState(0);
-
-    if (!dish) {
-        return null;
-    }
+export const Ingredient = ({ingredient}) => {
+    const [count, setCount] = useState(1);
 
     return <div>
-        {dish.name}
+        {ingredient}
         <div>
             <Button onClick={() => setCount(count - 1)}>-</Button>
             {count >= 0 && count <= 5 ? count : 'You can only add from 0-5'}
             <Button onClick={() => setCount(count + 1)}>+</Button>
-            {
-                count > 0 && <Ingredients ingredients={dish.ingredients} />
-            }
         </div>
     </div>
 }

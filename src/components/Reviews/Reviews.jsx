ff0c1239aee1a89199b9cf9b68a78102769b11fb
Anton Elshin <anton.elshin@mail.ru>
2023-01-09 22:22:30 +0300

    HW2 - format by prettier

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,14 +1,16 @@
-import {Review} from "../Review/Review";
+import { Review } from '../Review/Review';
 
-export const Reviews = ({reviews}) => {
-    return <div>
-        <h2>Reviews</h2>
-        <ul>
-            {
-                reviews.map((review) => <li>
-                    <Review review={review}/>
-                </li>)
-            }
-        </ul>
+export const Reviews = ({ reviews }) => {
+  return (
+    <div>
+      <h2>Reviews</h2>
+      <ul>
+        {reviews.map((review) => (
+          <li>
+            <Review review={review} />
+          </li>
+        ))}
+      </ul>
     </div>
-}
+  );
+};

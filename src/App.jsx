394610bc17c3fe8fc198e7284706792a23918e69
Maxim <Sen989@yandex.ru>
2023-01-12 01:15:25 +0700

    Create Tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { restaurants } from './constants/fixtures';
 import { Restaurant } from './components/Restaurant/Restaurant';
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs } from './components/Tabs/Tabs';
+
+
 
 export const App = () => {
+  const[isIndex, setisIndex] = useState();
+  function show(id){
+    setisIndex(id)
+  }
+  
   return (
     <div>
+      <Tabs onClick={show}/>
+      {restaurants.map((restaurant, index) =>
+          <div>
+                {index === isIndex ? <Restaurant restaurant={restaurant}/> : null }
+          </div>
+            
+        )}
       {/*<Tabs />*/}
       {/*<Restaurant restaurant={activeRestaurant} />*/}
+      
     </div>
   );
 };

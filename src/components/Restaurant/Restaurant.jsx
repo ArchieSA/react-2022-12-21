f60f0cee8b7036509f5731770927cfab1a540130
Anton Elshin <anton.elshin@mail.ru>
2023-01-31 14:45:02 +0300

    HW8

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,15 +1,40 @@
 import { useSelector } from 'react-redux';
-import { selectRestaurantById } from '../../store/modules/restaurant/selectors';
-import { Outlet, useParams } from 'react-router-dom';
-import React from 'react';
+import {
+  selectRestaurantById,
+  selectRestaurantMenuById,
+} from '../../store/modules/restaurant/selectors';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 import { Tabs } from '../Tabs/Tabs';
+import { selectDishIds } from '../../store/modules/dish/selectors';
 
 export const Restaurant = () => {
   const { restaurantId } = useParams();
+  const navigate = useNavigate();
   const restaurant = useSelector((state) =>
     selectRestaurantById(state, { restaurantId })
   );
+
+  const loadedDishIds = useSelector((state) => selectDishIds(state));
+  const restaurantDishIds = useSelector((state) =>
+    selectRestaurantMenuById(state, { restaurantId })
+  );
+
+  let needShowMenu = false;
+  if (
+    restaurantDishIds.every((restaurantDishId) =>
+      loadedDishIds.includes(restaurantDishId)
+    )
+  ) {
+    needShowMenu = true;
+  }
+
+  useEffect(() => {
+    if (!needShowMenu) {
+      navigate('menu');
+    }
+  }, [needShowMenu]);
 
   if (!restaurant) {
     return null;

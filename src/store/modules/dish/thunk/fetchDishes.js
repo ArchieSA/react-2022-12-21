8c083a4916896ad23f3661366400526b1284dddd
Anton Elshin <anton.elshin@mail.ru>
2023-02-05 14:46:50 +0300

    HW8 filter dishes by name with useSearchParams

--- src/store/modules/dish/thunk/fetchDishes.js
+++ src/store/modules/dish/thunk/fetchDishes.js
@@ -1,17 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { selectDishIds } from '../selectors';
-import { LOADING_STATUSES } from '../../../constants/loadingStatuses';
 
 export const fetchDishes = createAsyncThunk(
   `dish/fetchDishes`,
-  async (_, { getState, rejectWithValue }) => {
-    console.log('fetchRestaurants');
-    const state = getState();
-
-    /*    if (selectDishIds(state)?.length) {
-      return rejectWithValue(LOADING_STATUSES.earlyAdded);
-    }*/
-
+  async (_, {}) => {
     const response = await fetch('http://localhost:3001/api/products');
 
     return await response.json();
